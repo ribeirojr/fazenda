321d79516113ac2ae2ab335147f9ca79db578a8e
ribeirojr <ribeirojr@gmail.com>
2011-12-16 14:12:14 +0000

    Update src/fazenda/views/index.clj

--- src/fazenda/views/index.clj
+++ src/fazenda/views/index.clj
@@ -14,4 +14,5 @@
    [:p "Foi nesta expedição que encontraram no lago da fazenda três submarinos da segunda guerra. Os soldados estavam vivos e nem sabiam que a guerra havia acabado."]
    [:p "Teve um churrasco na fazenda do LW. Era tanta carne que duas espécies de boi entraram em extinção. E quando ligaram as luzes teve blecaute no planalto central."]
    [:p "A fazenda é tão grande que tem duas dimensões paralelas. E lá foi filmado LOST."]
-   [:p "Certa vez, a fazenda do LW foi invadida por sem-terra. Eram pigmeus africanos."]))
+   [:p "Certa vez, a fazenda do LW foi invadida por sem-terra. Eram pigmeus africanos."]
+   [:p "A fazenda do LW é a unica no estado de Goias a ter vista para o mar."]))
